5842632694870bfadeb6bf6ed4dfadd74cbbc790
ckdduf138 <ckdduf138@naver.com>
2025-10-19 14:01:37 +0900

    test: #36 빌드 콘솔 로그 추가

--- src/components/Layout/UserAvatar.tsx
+++ src/components/Layout/UserAvatar.tsx
@@ -53,7 +53,6 @@ export const UserAvatarWithAuth = () => {
     return <div className="w-16 h-8 animate-pulse bg-gray-200 rounded dark:bg-gray-700"></div>;
   }
 
-  
   return isAuthenticated ? <UserAvatar /> : <AuthButton />;
 };
 export default UserAvatarWithAuth;
