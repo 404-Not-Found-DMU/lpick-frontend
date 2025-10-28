e7279634bc7469bc2da112a61a356a5d3096fe1d
ckdduf138 <ckdduf138@naver.com>
2025-10-28 19:22:44 +0900

    fix: 중복 import 제거

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -4,7 +4,6 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ToastProvider } from './Toast/ToastProvider';
 import { ReactNode, useState } from 'react';
-import { ToastProvider } from './Toast/ToastProvider';
 
 interface ProvidersProps {
   children: ReactNode;
