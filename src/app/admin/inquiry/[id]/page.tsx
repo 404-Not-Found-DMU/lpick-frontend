ddcb8e974f9c8939239612cd38910ecdd00982d4
Gun06 <kogun0626@naver.com>
2025-11-18 01:48:34 +0900

    fix: #78 import 경로 오류 수정

--- src/app/admin/inquiry/[id]/page.tsx
+++ src/app/admin/inquiry/[id]/page.tsx
@@ -1,4 +1,4 @@
-import InquiryAdminDetailClient from './parts/InquiryAdminDetailClient'
+import InquiryAdminDetailClient from '../parts/InquiryAdminDetailClient'
 
 export default function AdminInquiryDetailPage({ params }: { params: { id: string } }) {
   return <InquiryAdminDetailClient questionId={decodeURIComponent(params.id)} />
